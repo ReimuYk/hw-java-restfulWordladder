d524ed9992975ea93816c775acdca088baf59a78
yuki.h <yuki.h@sjtu.edu.cn>
2018-03-30 19:59:00 +0800

    spring boot init

---
+++ src/main/java/com/restwl/RunApplication.java
@@ -0,0 +1,15 @@
+package com.restwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by qianlong on 16/7/20.
+ */
+@SpringBootApplication
+public class RunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+    }
+}
