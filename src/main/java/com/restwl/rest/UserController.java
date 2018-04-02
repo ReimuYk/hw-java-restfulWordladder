5185af0cf54f91ccce0377904b51cd9dae60184a
yuki.h <yuki.h@sjtu.edu.cn>
2018-04-02 16:32:54 +0800

    log

--- src/main/java/com/restwl/rest/UserController.java
+++ src/main/java/com/restwl/rest/UserController.java
@@ -4,11 +4,20 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class UserController {
 
-    @RequestMapping(value = "/wordladder",method = RequestMethod.GET)//,consumes="application/json")
+    protected static Logger logger=LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/wordladder",method = RequestMethod.GET)
     public String getLadder(@RequestParam String w1,@RequestParam String w2) {
-        return "Welcome," + w1 + "--->" + w2 + wl.wordladder.runMain(w1,w2);
+        logger.debug("访问Wordladder");
+        return wl.wordladder.runMain(w1,w2);
     }
 }
